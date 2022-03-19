007c17b81b6a0033d24ed680429c2b379d05b997
tiger <panghu.lee@gmail.com>
2022-03-19 23:11:52 +0800

    [update]demo code.

--- Sources/XCActivityLogParserApp/main.swift
+++ Sources/XCActivityLogParserApp/main.swift
@@ -9,13 +9,4 @@ import Foundation
 
 import IDEActivityModel
 
-let log = try XCActivityLogParser().parseInPath("/Users/tiger/Downloads/255AEDF1-C132-4F9B-9853-7F20AA13D445.xcactivitylog")
-print(log)
-
-
-
-print(#function)
-
-//let ref = CCXCActivityLogParserCreate("/Users/tiger/Downloads/255AEDF1-C132-4F9B-9853-7F20AA13D445.uxcactivitylog")
-//
-//print(ref)
+print("hello world")
