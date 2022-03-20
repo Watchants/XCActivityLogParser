834b073f6ea2dd5b3f14ef9ab75896d7db5e86fd
tiger <panghu.lee@gmail.com>
2022-03-20 22:17:46 +0800

    [rename]XCActivityLogParser -> xcactivitylog-parser

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "XCActivityLogParser",
+    name: "xcactivitylog-parser",
     products: [
         .executable(
             name: "XCActivityLogParserApp",
