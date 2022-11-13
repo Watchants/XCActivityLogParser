0e18c9e0be0f1e3d1b7e9dfcce23aef6f15ae317
tiger <panghu.lee@gmail.com>
2022-11-13 14:04:21 +0800

    [tests] import

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -1,7 +1,11 @@
 import XCTest
+import Foundation
+
+@testable import IDEActivityModel
 @testable import XCActivityLogParser
 
 final class XCActivityLogParserTests: XCTestCase {
+    
     func testExample() throws {
         
     }
